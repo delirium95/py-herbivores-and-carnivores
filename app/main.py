2f026db39769e83b499e3e03755b3e876a47b514
Nikita Grinyov <nikita3grynov@gmail.com>
2025-09-22 13:28:14 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,29 +4,30 @@ from __future__ import annotations
 
 class Animal:
     alive = []
-    def __init__(self, name, hidden=False):
+
+    def __init__(self, name: str, hidden: bool = False) -> None:
         self.health = 100
         self.name = name
         self.hidden = hidden
         Animal.alive.append(self)
 
-    def __repr__(self):
-        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+    def __repr__(self) -> str:
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, Hidden: {self.hidden}}}")
 
-    def die(self):
+    def die(self) -> None:
         if self in Animal.alive:
             Animal.alive.remove(self)
 
 
 class Herbivore(Animal):
-    def hide(self):
+    def hide(self) -> None:
         self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
-    def bite(self, herbivore: Herbivore):
+    def bite(self, herbivore: Herbivore) -> None:
         if not herbivore.hidden:
             herbivore.health -= 50
             if herbivore.health <= 0:
                 herbivore.die()
-
